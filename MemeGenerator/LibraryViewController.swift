f76899c573f9785dee88ced6274f0e37c0b2276f
Илья Мудрый <mudriy.ilya@gmail.com>
2021-09-04 01:27:57 +0300

    initial commit

---
+++ MemeGenerator/LibraryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LibraryViewController.swift
+//  MemeGenerator
+//
+//  Created by Илья Мудрый on 03.09.2021.
+//
+
+import UIKit
+
+class LibraryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
